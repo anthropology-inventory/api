101f1ee6d41541bd17765eeeab55e2b03ca79be5
Alexander Ruban <alexruban29@gmail.com>
2026-03-15 17:12:17 -0700

    Created Admin middleware and updated the auth.js router.

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const { signup, login } = require('../controllers/authController.js');
 const requireAuth = require('../middleware/authMiddleware.js');
+const requireAdmin = require('../middleware/requireAdminMiddleware.js');
 
 const authRouter = Router();
 
-// new user accounts must be manually created via postman using
-// the existing dev / admin user account JWT in the db
-authRouter.post('/signup', requireAuth, signup);
+// new user accounts can only be created by authenticated admin users
+authRouter.post('/signup', requireAuth, requireAdmin, signup);
 
 authRouter.post('/login', login);
 
